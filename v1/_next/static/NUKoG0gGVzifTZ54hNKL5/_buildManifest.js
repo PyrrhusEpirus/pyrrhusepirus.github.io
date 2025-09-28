e034e15d1de83fa161a6bc0dc436ad91421c2a5f
Pyrrhus Epirus <pyrrostisipeirou@gmail.com>
2025-09-27 22:49:21 -0400

    Testing v1 statically generated website

---
+++ v1/_next/static/NUKoG0gGVzifTZ54hNKL5/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/36e709269d62b8a2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ v1/_next/static/chunks/c89e2a9963ec5c77.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/4851f5e9d970d43f.js",
+  "static/chunks/bbf6c57c16a774f1.js",
+  "static/chunks/turbopack-985a3172540523f3.js"
+])
